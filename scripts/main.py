7582e03338a7be1f4a264c00e45731adb01660da
T_cat9000_2 <terpstragraham@gmail.com>
2024-10-06 14:22:31 -0700

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,37 @@
 import requests
 
-url = 'https://webbrowser11.github.io/backend/cinnamon.txt'
-response = requests.get(url)
+url = 'https://webbrowser11.github.io/backend/banner.txt'
+urlfetch()
+class Main:
+    def urlfetch()
+        response = requests.get(url)
+        
+        content = response.text  # Get the content of the page
+        print(content)
+    
+    def welcome():
+        print("you reached the webbrwoser11 BACKEND!")
+        print("here is a list of files you can open view now:")
+        print("cinnamon")
+        print("readme")
+        print("or enter a command:")
+        print("banner - show the banner")
+        print("welcome - shows the welcome message")
+    
+    def script()
+        input = input("file/command: ")
+        if input == "banner":
+            url = 'https://webbrowser11.github.io/backend/banner.txt'
+            urlfetch()
+        elif input == "welcome":
+            welcome()
+        elif input == "cinnamon":
+            url = 'https://webbrowser11.github.io/backend/cinnamon.txt'
+            urlfetch()
+        elif input == "readme":
+            url = 'https://webbrowser11.github.io/backend/readme.md'
+            urlfetch()
 
-if response.status_code == 200:  # Check if the request was successful
-    content = response.text  # Get the content of the page
-    print(content)
-else:
-    print(f"Failed to retrieve the page. Status code: {response.status_code}")
+
+if __name__ == "__main__":
+    Main()
