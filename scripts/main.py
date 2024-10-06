ab6c399a9cac54eba10e935d4f652bddd20e14f5
T_cat9000_2 <terpstragraham@gmail.com>
2024-10-06 14:55:19 -0700

    fix

--- scripts/main.py
+++ scripts/main.py
@@ -1,36 +1,38 @@
 import requests
 
-url = 'https://webbrowser11.github.io/backend/banner.txt'
-urlfetch()
 class Main:
-    def urlfetch()
-        response = requests.get(url)
-        
-        content = response.text  # Get the content of the page
-        print(content)
-    
-    def welcome():
-        print("you reached the webbrwoser11 BACKEND!")
-        print("here is a list of files you can open view now:")
-        print("cinnamon")
-        print("readme")
-        print("or enter a command:")
-        print("banner - show the banner")
-        print("welcome - shows the welcome message")
-    
-    def script()
-        input = input("file/command: ")
-        if input == "banner":
-            url = 'https://webbrowser11.github.io/backend/banner.txt'
-            urlfetch()
-        elif input == "welcome":
-            welcome()
-        elif input == "cinnamon":
-            url = 'https://webbrowser11.github.io/backend/cinnamon.txt'
-            urlfetch()
-        elif input == "readme":
-            url = 'https://webbrowser11.github.io/backend/readme.md'
-            urlfetch()
+    def script():
+        def urlfetch():
+            response = requests.get(url)
+            
+            content = response.text  # Get the content of the page
+            print(content)
+        def welcome():
+            print("you reached the webbrwoser11 BACKEND!")
+            print("here is a list of files you can open view now:")
+            print("cinnamon")
+            print("readme")
+            print("or enter a command:")
+            print("banner - show the banner")
+            print("welcome - shows the welcome message")
+
+        url = 'https://webbrowser11.github.io/backend/banner.txt'
+        urlfetch()
+        welcome()
+        while True:
+            user = input("file/command: ")
+            if user == "banner":
+                url = 'https://webbrowser11.github.io/backend/banner.txt'
+                urlfetch()
+            elif user == "welcome":
+                welcome()
+            elif user == "cinnamon":
+                url = 'https://webbrowser11.github.io/backend/cinnamon.txt'
+                urlfetch()
+            elif user == "readme":
+                url = 'https://webbrowser11.github.io/backend/README.md'
+                urlfetch()
+    script()
 
 
 if __name__ == "__main__":
